7e189893fae42c63d29c4f8e5df9070ce499178f
bellavickanova <vichkanovaby@m2.ru>
2024-09-19 23:14:56 +0300

    feat: test hello world from me

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -3,6 +3,6 @@ import org.junit.jupiter.api.Test;
 public class HelloWorldTest {
     @Test
     public void TestHelloWorld(){
-        System.out.println("hello world");
+        System.out.println("hello world from Bella Vichkanova");
     }
 }
