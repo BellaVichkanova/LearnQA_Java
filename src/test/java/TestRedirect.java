f6b69b68395806bf790a44881dad931e7a7024ef
bellavickanova <vichkanovaby@m2.ru>
2024-09-29 00:55:33 +0300

    feat: test long redirect

---
+++ src/test/java/TestRedirect.java
@@ -0,0 +1,30 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class TestRedirect {
+    String urlParam = "https://playground.learnqa.ru/api/long_redirect";
+    int redirectCount = 1;
+    @Test
+    public  void getRedirect() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get(urlParam)
+                .andReturn();
+        int StatusCodeResponse = response.getStatusCode();
+        urlParam = response.getHeader("Location");
+        if (StatusCodeResponse != 200) {
+            System.out.println("\nРедирект №" + redirectCount);
+            System.out.println(urlParam);
+            redirectCount++;
+            getRedirect();
+        }
+        else {System.out.println("\nКод ответа сервера:" + StatusCodeResponse);}
+
+
+
+    }
+}
