3ea1557525dc1d91bcaaa63f7293c4b633384d54
bellavickanova <vichkanovaby@m2.ru>
2024-10-16 23:14:58 +0300

    feat: ex14 create framework

--- src/test/java/HelloWorldTest.java
+++
@@ -1,8 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class HelloWorldTest {
-    @Test
-    public void TestHelloWorld(){
-        System.out.println("hello world from Bella Vichkanova");
-    }
-}
